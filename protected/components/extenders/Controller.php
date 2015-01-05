a5e73e8f93ce0219c2dfea775eb92ccff7533b6d
glen <jsjgjf@qq.com>
2015-01-05 09:52:41 +0800

    修改

--- protected/components/extenders/Controller.php
+++ protected/components/extenders/Controller.php
@@ -1,24 +1,59 @@
 <?php
-/**
- * Controller is the customized base controller class.
- * All controller classes for this application should extend from this base class.
- */
-class Controller extends CController
-{
-	/**
-	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
-	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
-	 */
-	public $layout='//layouts/column1';
-	/**
-	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
-	 */
-	public $menu=array();
-	/**
-	 * @var array the breadcrumbs of the current page. The value of this property will
-	 * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
-	 * for more details on how to specify this property.
-	 */
-	public $breadcrumbs=array();
+class Controller extends CController {
 	
+	public $menu = array ();
+	public $breadcrumbs = array ();
+	public $column;
+	public $category=array();
+	public $metaKeywork;
+	public $metaDescription;
+	
+	public function beforeAction($action) {
+		Yii::app ()->cacheManage->cacheName = 'ContentCache';
+		$this->menu = Yii::app ()->cacheManage->findCache ();
+		Yii::app ()->cacheManage->cacheName = 'ClassifyCache';
+		$this->category = Yii::app ()->cacheManage->findCache ();
+		Yii::app ()->clientScript->registerCoreScript ( 'jquery' )
+        //->registerScriptFile ( Yii::app ()->baseUrl . '/scripts/ jquery.mobile.min.js' )
+        ->registerScriptFile ( Yii::app ()->baseUrl . '/scripts/function.js' )
+		->registerCssFile ( Yii::app ()->baseUrl . '/css/style.css' );
+		
+		if(isset($_GET['keyparam'])){
+			foreach ($this->menu as $v){
+				if($v['keyparam']==$_GET['keyparam']){
+					$this->column=$v;
+					break;
+				}
+			}
+		}
+		
+		return parent::beforeAction ( $action );
+	}
+	
+	public function beforeRender($view){
+		if(isset($_GET['id'])){
+			Yii::app()->cacheManage->cacheName='ModelCache';
+			$model_cache=Yii::app()->cacheManage->findCache();
+			$model_now=$model_cache[$this->column['modelid']];
+			$info=BaseModel::model($model_now['object'])->findByPk((int)$_GET['id']);
+			$this->pageTitle=$info->title.' - '.$this->column['colname'].' - '.SettingGet::get('base', 'title');
+			$cs=Yii::app()->clientScript;
+			$cs->registerMetaTag($info->keywords.','.SettingGet::get('base', 'title'),'keywords');
+			$cs->registerMetaTag($info->description.' '.SettingGet::get('base', 'title'),'description');
+		}
+		elseif(isset($_GET['keyparam'])){
+			$this->pageTitle=$this->column['colname'].' - '.SettingGet::get('base', 'title');
+			$seo_info=unserialize($this->column['setting']);
+			$cs=Yii::app()->clientScript;
+			$cs->registerMetaTag($seo_info['metaKeyword'].','.SettingGet::get('base', 'title'),'keywords');
+			$cs->registerMetaTag($seo_info['metaDescription'].' '.SettingGet::get('base', 'title'),'description');
+		}
+		else{
+			$this->pageTitle=SettingGet::get('base', 'title').(SettingGet::get('base', 'subTitle')!=''?' - '.SettingGet::get('base', 'subTitle'):'');
+			$cs=Yii::app()->clientScript;
+			$cs->registerMetaTag(SettingGet::get('base', 'metaKeyword'),'keywords');
+			$cs->registerMetaTag(SettingGet::get('base', 'metaDescription'),'description');
+		}		
+		return parent::beforeRender($view);
+	}
 }
